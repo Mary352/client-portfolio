4d4d0a12b44c6eb5fff4be70454c1ac31da158a0
Mary <mary.sk2000@gmail.com>
2025-01-15 05:31:52 +0300

    feat(js): add post fetch to contacts form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './index.html';
 import './normalize.min.css';
 import './index.scss';
+
+const contactsSubmitBtn = document.getElementById('contacts__submit-btn');
+const BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+contactsSubmitBtn.addEventListener('click', async (e) => {
+   e.preventDefault();
+
+   const name = document.getElementById('username').value;
+   const email = document.getElementById('email').value;
+   const text = document.getElementById('text').value;
+
+   const body = { name, email, body: text }
+   const url = BASE_URL + '/posts/1/comments'
+   let res = await fetch(url, {
+      method: 'POST',
+      headers: {
+         'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify(body)
+   })
+
+   const result = await res.json()
+   console.log("🚀 ~ contactsSubmitBtn.addEventListener ~ result:", result)
+})
