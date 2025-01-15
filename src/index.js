062c88e585ddcbdbe759cc25eb748b8061f790e2
Mary <mary.sk2000@gmail.com>
2025-01-15 06:40:39 +0300

    feat: add contacts form validation

--- src/index.js
+++ src/index.js
@@ -2,17 +2,32 @@ import './index.html';
 import './normalize.min.css';
 import './index.scss';
 
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+const emailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 const contactsSubmitBtn = document.getElementById('contacts__submit-btn');
-const BASE_URL = 'https://jsonplaceholder.typicode.com'
+const errorName = document.getElementById('contacts__input-error-name');
+const errorEmail = document.getElementById('contacts__input-error-email');
+const errorTextarea = document.getElementById('contacts__input-error-textarea');
+const nameInput = document.getElementById('username');
+const emailInput = document.getElementById('email');
+const textInput = document.getElementById('text');
 
-contactsSubmitBtn.addEventListener('click', async (e) => {
-   e.preventDefault();
+function showError(block, errorText) {
+   block.textContent = errorText;
+   block.style.display = 'block';
+}
 
-   const name = document.getElementById('username').value;
-   const email = document.getElementById('email').value;
-   const text = document.getElementById('text').value;
+function hideBlock(block) {
+   block.style.display = 'none';
+}
 
-   const body = { name, email, body: text }
+function addFocusListener(input, block) {
+   input.addEventListener('focus', () => {
+      hideBlock(block);
+   })
+}
+
+async function sendForm(body) {
    const url = BASE_URL + '/posts/1/comments'
    let res = await fetch(url, {
       method: 'POST',
@@ -24,4 +39,36 @@ contactsSubmitBtn.addEventListener('click', async (e) => {
 
    const result = await res.json()
    console.log("🚀 ~ contactsSubmitBtn.addEventListener ~ result:", result)
+}
+
+
+contactsSubmitBtn.addEventListener('click', (e) => {
+   e.preventDefault();
+
+   const name = nameInput.value;
+   const email = emailInput.value;
+   const text = textInput.value;
+
+   if (!name) {
+      showError(errorName, 'Введите ваше имя');
+      return;
+   }
+
+   if (!emailRegex.test(email)) {
+      showError(errorEmail, 'Неверная почта');
+      return;
+   }
+
+   if (!text) {
+      showError(errorTextarea, 'Введите ваше сообщение');
+      return;
+   }
+
+   const body = { name, email, body: text }
+   sendForm(body);
 })
+
+addFocusListener(nameInput, errorName);
+addFocusListener(emailInput, errorEmail);
+addFocusListener(textInput, errorTextarea);
+
